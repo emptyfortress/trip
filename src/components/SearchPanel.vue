deeda30beecb58f80bb52ed4ebf0459cf1fb1f45
Volkov Alexander <volkov.525@gmail.com>
2020-01-16 14:48:46 +0300

    finish search

--- src/components/SearchPanel.vue
+++ src/components/SearchPanel.vue
@@ -1,13 +1,12 @@
 <template lang="pug">
 .d-flex.justify-start.mt-5
-	//- v-container
 	.pan
 		.mx-5
 		.zag Настройки поиска
 		v-item-group(mandatory)
 			v-row
 				v-col(cols="12" md="6")
-					v-item(v-slot:default="{active, toggle}")
+					v-item(v-slot:default="{active, toggle}" )
 						v-card(:color="active ? 'primary' : '' " height="42" @click="toggle").toggle
 							.txt В текущей папке
 							v-scroll-y-transition(mode="out-in")
@@ -20,11 +19,26 @@
 							v-scroll-y-transition(mode="out-in")
 								.act(v-if="active")
 									v-icon(color="white" large) mdi-check-bold
-	.mx-5
-		.zag История
-		ul
-			li(v-for="n in 10")
-				a(href="").reverse Текст запроса в поле поиска
+		v-item-group(v-model="model")
+			v-row
+				v-col(cols="12" sm="6")
+					v-item(v-slot:default="{active, toggle}")
+						v-card(:color="active ? 'primary' : '' " height="42" @click="test").toggle
+							.txt Расширенный поиск по атрибутам
+							v-scroll-y-transition(mode="out-in")
+								.act(v-if="active")
+									v-icon(color="white" large) mdi-check-bold
+
+	.pan
+		transition(name="slide-fade" mode="out-in")
+			.mx-5(v-if="!advanced" key="1")
+				.zag История
+				ul
+					li(v-for="n in 10")
+						a(href="").reverse Текст запроса в поле поиска
+			.mx-5(v-else key="2")
+				.zag Панель расширенного поиска
+				v-card.panel
 
 </template>
 
@@ -33,7 +47,21 @@
 export default {
 	data () {
 		return {
-
+			advanced: false,
+			model: null
+		}
+	},
+	methods: {
+		test () {
+			this.advanced = !this.advanced
+			switch (this.model) {
+			case null:
+				this.model = 0
+				break
+			case 0:
+				this.model = null
+				break
+			}
 		}
 	}
 }
@@ -63,6 +91,10 @@ export default {
 	position: absolute;
 	top: .1rem;
 	right: 1rem;
+}
+.panel {
+	width: 100%;
+	height: 500px;
 }
 
 </style>
