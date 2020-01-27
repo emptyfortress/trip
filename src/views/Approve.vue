f485d3410add4e3d50cbdc747201aa319ff4e604
Volkov Alexander <volkov.525@gmail.com>
2020-01-27 12:56:43 +0300

    fix

--- src/views/Approve.vue
+++ src/views/Approve.vue
@@ -1,10 +1,12 @@
 <template lang="pug">
 v-row(justify="center").mx-3
 	v-col(md="12" lg="9")
-		.zag
-			span Согласование:
-			|  командировка
-		br
+		v-row(justify="space-between")
+			v-col
+				.zag.mb-2
+					span Согласование:
+					|  командировка
+			v-col(cols="2").status В работе
 		v-tabs.mytab
 			v-tab Главная
 			v-tab Подчиненные задания
@@ -40,8 +42,16 @@ export default {
 	color: black;
 	text-align: left;
 	font-size: 1.5rem;
+	/* white-space: nowrap; */
 	span {
 		opacity: .5;
 	}
+}
+.status {
+	font-size: 1.2rem;
+	text-align: right;
+	margin-top: .6rem;
+	/* width: 120px; */
+	/* background: #ccc; */
 }
 </style>
